cac767b5ca30950bd31d4140f8c3e179503586fd
Matheus Dias <matheus.dias@ifood.com.br>
2020-01-30 13:52:40 -0300

    Movendo peças

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,13 +3,29 @@ package application;
 import boardgame.Board;
 import boardgame.Position;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
 
-    ChessMatch chessMath = new ChessMatch();
-    UI.printBoard(chessMath.getPieces());
+        Scanner sc = new Scanner(System.in);
+        ChessMatch chessMath = new ChessMatch();
 
+        while (true) {
+            UI.printBoard(chessMath.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMath.perfomChessMove(source, target);
+        }
     }
 
 }
