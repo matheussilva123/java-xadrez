c30bed6b52060bc8d0757c0a1aa8be9de4640812
Matheus Dias <matheus.dias@ifood.com.br>
2020-01-31 09:38:14 -0300

    logica checkmat

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,8 +18,10 @@ public class Program {
         Scanner sc = new Scanner(System.in);
 
         ChessMatch chessMatch = new ChessMatch();
+
         List<ChessPiece> captured = new ArrayList<>();
-        while (true) {
+
+        while (!chessMatch.getCheckMate()) {
             try {
                 UI.clearScreen();
                 UI.printMatch(chessMatch, captured);
@@ -48,5 +50,7 @@ public class Program {
                 sc.nextLine();
             }
         }
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
     }
 }
