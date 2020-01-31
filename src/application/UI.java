073dffbad30537735ce19ff98eec94bff4f0b471
Matheus Dias <matheus.dias@ifood.com.br>
2020-01-31 08:54:26 -0300

    trocando de jogador a cada turno

--- src/application/UI.java
+++ src/application/UI.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
+import chess.ChessMatch;
+
 
 public class UI {
 
@@ -46,6 +48,13 @@ public class UI {
         catch (RuntimeException e) {
             throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
         }
+    }
+
+    public static void printMatch(ChessMatch chessMatch) {
+        printBoard(chessMatch.getPieces());
+        System.out.println();
+        System.out.println("Turn : " + chessMatch.getTurn());
+        System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
     }
 
     public static void printBoard(ChessPiece[][] pieces) {
